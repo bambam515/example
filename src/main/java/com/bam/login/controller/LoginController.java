364e6a572fa817ab76e8c0e4f3c4e9f47ff74c40
bambam515 <tqi7415tq@naver.com>
2021-11-16 08:58:08 +0900

    로그아웃

--- src/main/java/com/bam/login/controller/LoginController.java
+++ src/main/java/com/bam/login/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.bam.login.controller;
 public class LoginController {
 	public static void main(String args[]) {
 		System.out.println("Login");
+		System.out.println("Log-Out");
 	}
 
 }
